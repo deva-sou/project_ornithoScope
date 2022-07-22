b25106bf1b30574229e99920f9304eed827c3071
gdamms <damguillotin@gmail.com>
2022-07-22 19:22:26 +0200

    hisotry works good

--- src/history.py
+++ src/history.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pickle
+import argparse
 
 
 argparser = argparse.ArgumentParser(
@@ -8,7 +9,7 @@ argparser = argparse.ArgumentParser(
 argparser.add_argument(
     '-p',
     '--pick',
-    default='data/pickles/history/history_data/saved_weights/new_weights/MobileNet_caped300_data_aug_v0_ADAM_bestLoss.h5.p',
+    default='data/pickles/history/history_data/saved_weights/new_weights/MobileNet_caped300_data_aug_v0_RMS2_bestLoss.h5.p',
     help='Path to pickle file.')
 
 
@@ -17,8 +18,8 @@ def _main_(args):
     with open(pickle_file_path, 'rb') as input_file:
         history = pickle.load(input_file)
         
-    loss = histroy['loss']
-    val_loss = hisotry['val_loss']
+    loss = history['loss']
+    val_loss = history['val_loss']
 
     steps = [i for i in range(len(loss))]
 
@@ -31,3 +32,50 @@ def _main_(args):
 if __name__ == '__main__':
     _args = argparser.parse_args()
     _main_(_args)
+
+
+
+# import matplotlib.pyplot as plt
+# import pickle
+# import argparse
+
+
+# argparser = argparse.ArgumentParser(
+#     description='Plot training loss and validation loss hisotiry.')
+
+# argparser.add_argument(
+#     '-p',
+#     '--pick',
+#     default='data/pickles/history/history_data/saved_weights/new_weights/MobileNet_caped300_data_aug_v0_$#_bestLoss.h5.p',
+#     help='Path to pickle file.')
+
+
+# def _main_(args):
+#     pickle_file_path = args.pick
+
+#     patches = {'$' : ['ADAM', 'RMS'], '#' : ['0', '1', '2', '3', '4']}
+
+#     loss = []
+#     val_loss = []
+#     steps = []
+#     for key in patches.keys():
+#         for val in patches[key]:
+#             current path = val.join(pickle_file_path.split(key))
+
+#             with open(pickle_file_path, 'rb') as input_file:
+#                 history = pickle.load(input_file)
+                
+#             loss.append(history['loss'])
+#             val_loss.append(history['val_loss'])
+
+#             steps.append([i for i in range(len(loss))])
+
+#     plt.figure('Histories')
+#     plt.plot(steps, loss)
+#     plt.plot(steps, val_loss)
+#     plt.show()
+
+
+# if __name__ == '__main__':
+#     _args = argparser.parse_args()
+#     _main_(_args)
