f40ea943dc44b3c4ff75519c397b7f20ad0208e3
deva-sou <soudeva974@gmail.com>
2022-07-28 09:47:58 +0200

    mosaic

--- src/keras_yolov2/preprocessing.py
+++ src/keras_yolov2/preprocessing.py
@@ -391,26 +391,25 @@ class BatchGenerator(Sequence):
         
         '''Encore à adapter: changer les img_path par peut-être image et aussi les bbox'''
         if jitter=='mosaic': #si on utilise l'augmentation de données mosaic
-                    #from lxml import etree
-#from ipdb import set_trace
+            
             OUTPUT_SIZE = (1024, 1024) # Height, Width
             SCALE_RANGE = (0.5, 0.5)
             FILTER_TINY_SCALE = 1 / 50 # if height or width lower than this scale, drop it.
-#voc Data set in format ,anno_dir It's a label xml file ,img_dir It's corresponding to jpg picture 
+            #voc Data set in format ,anno_dir It's a label xml file ,img_dir It's corresponding to jpg picture 
             ANNO_DIR = 'data/inputs/input_all.csv'
             IMG_DIR ='data/inputs/raw_data/cleaned_labels/input_train_caped300_cleaned.csv'
-# category_name = ['background', 'person']
+            # category_name = ['background', 'person']
 
 
             def main():
                 img_paths, annos = get_dataset(ANNO_DIR, IMG_DIR)
-    # set_trace()
+    
                 idxs = random.sample(range(len(annos)), 4)# from annos Take... Randomly from the list length 4 Number 
-    # set_trace()
+    
                 new_image, new_annos = update_image_and_anno(img_paths, annos, idxs, OUTPUT_SIZE, SCALE_RANGE, filter_scale=FILTER_TINY_SCALE)
-    # Update to get new graph and corresponding data anno
+                # Update to get new graph and corresponding data anno
                 cv2.imwrite('./img/wind_output.jpg', new_image)
-    #annos yes 
+                #annos yes 
                 for anno in new_annos:
                     start_point = (int(anno[1] * OUTPUT_SIZE[1]), int(anno[2] * OUTPUT_SIZE[0]))# Top left corner 
                     end_point = (int(anno[3] * OUTPUT_SIZE[1]), int(anno[4] * OUTPUT_SIZE[0]))# Lower right corner 
@@ -418,8 +417,8 @@ class BatchGenerator(Sequence):
                     cv2.imwrite('./img/wind_output_box.jpg', new_image)
                     new_image = cv2.cvtColor(new_image, cv2.COLOR_BGR2RGB)
                     new_image = Image.fromarray(new_image.astype(np.uint8))
-    # new_image.show()
-    # cv2.imwrite('./img/wind_output111.jpg', new_image)
+                    # new_image.show()
+                    # cv2.imwrite('./img/wind_output111.jpg', new_image)
             def update_image_and_anno(all_img_list, all_annos, idxs, output_size, scale_range, filter_scale=0.):
                 output_img = np.zeros([output_size[0], output_size[1], 3], dtype=np.uint8)
                 scale_x = scale_range[0] + random.random() * (scale_range[1] - scale_range[0])
@@ -428,7 +427,7 @@ class BatchGenerator(Sequence):
                 divid_point_y = int(scale_y * output_size[0])
                 new_anno = []
                 for i, idx in enumerate(idxs):
-        #set_trace()
+        
                     path = all_img_list[idx]
                     img_annos = all_annos[idx]
                     img = cv2.imread(path)
@@ -471,21 +470,21 @@ class BatchGenerator(Sequence):
                     return output_img, new_anno
 
             def get_dataset(anno_dir, img_dir):
-    # class_id = category_name.index('person')
+                # class_id = category_name.index('person')
                 img_paths = []
                 annos = []
-    # for anno_file in glob.glob(os.path.join(anno_dir, '*.txt')):
+                # for anno_file in glob.glob(os.path.join(anno_dir, '*.txt')):
                 for anno_file in glob.glob(os.path.join(anno_dir, '*.xml')):
-    # anno_id = anno_file.split('/')[-1].split('.')[0]
+                    # anno_id = anno_file.split('/')[-1].split('.')[0]
                     anno_id = anno_file.split('/')[-1].split('x')[0]
-    # set_trace()
-    # with open(anno_file, 'r') as f:
-    # num_of_objs = int(f.readline())
-    # set_trace()
+                    
+                    # with open(anno_file, 'r') as f:
+                    # num_of_objs = int(f.readline())
+   
                     img_path = os.path.join(img_dir, f'{anno_id}jpg')
                     print(img_path)
                     img = cv2.imread(img_path)
-    # set_trace() 
+   
                     img_height, img_width, _ = img.shape
                     print(img.shape)
                     del img
@@ -493,7 +492,7 @@ class BatchGenerator(Sequence):
                     bnd_box = parseXmlFiles(anno_file)
                     print(bnd_box)
                     for bnd_id, box in enumerate(bnd_box):
-    # set_trace()
+ 
                         categories_id = box[0]
                         xmin = max(int(box[1]), 0) / img_width
                         ymin = max(int(box[2]), 0) / img_height
