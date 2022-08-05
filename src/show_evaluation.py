dc9f090ebdfe5e3267db38772bc05c7ed718f3f9
gdamms <damguillotin@gmail.com>
2022-08-05 10:00:12 +0200

    feat: bad boxes

--- src/show_evaluation.py
+++ src/show_evaluation.py
@@ -6,12 +6,12 @@ import numpy as np
 
 from keras_yolov2.utils import draw_boxes
 
-pickle_path = "data/pickles/MobileNet_2022-08-01-15:36:48/boxes_MobileNet_task_20210705-07_balacet.p"
+pickle_path = "data/pickles/MobileNet_2022-08-05-09:02:01/boxes_MobileNet_input_test.p"
 
 with open(pickle_path, 'rb') as fp:
     img_boxes = pickle.load(fp)
 
-config_path = "config/config_to_train/ADAM_lr4.json"
+config_path = "config/pre_config/ADAM_OCS_v0_full_sampling.json"
 
 with open(config_path) as config_buffer:
     config = json.load(config_buffer)
@@ -26,9 +26,4 @@ for img in img_boxes:
     frame = cv2.imread(img_path)
     frame = draw_boxes(frame, img_boxes[img], config['model']['labels'])
     video_writer.write(np.uint8(cv2.resize(frame, (800, 600))))
-
-    # cv2.imshow('Prediction', cv2.resize(frame, (800, 600)))
-    # key = cv2.waitKey(0)
-    # if key == 27 or key == ord('q'):
-    #     break
 video_writer.release()
